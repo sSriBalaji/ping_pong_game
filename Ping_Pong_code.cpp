7fd03e6818a5bee2351ac886be6d61a958b6479f
sSriBalaji <sribalaji7474@gmail.com>
2025-01-13 19:47:30 +0530

    partial part of gameManager

--- Ping_Pong_code.cpp
+++ Ping_Pong_code.cpp
@@ -1,4 +1,7 @@
 #include<iostream>
+#include<time.h>
+#include<conio.h>
+#include<Windows.h>
 using namespace std;
 enum eDirection{STOP =0, LEFT = 1, UPLEFT =2,DOWNLEFT=3,RIGHT =4,UPRIGHT=5, DOWNRIGHT=6};
 class cBall{
@@ -73,8 +76,52 @@ class cPaddle{
         inline void reset(){x=originalX; y=originalY;}
         inline void moveUp(){y--;}
         inline void moveDown(){y++;}
-        
 
+
+};
+
+class cGameManager{
+    private:
+        int width;
+        int height;
+        int score1,score2;
+        char up1,down1,up2,down2;
+        bool quit;
+        cBall * ball;
+        cPaddle *player1;
+        cPaddle *player2;
+    public:
+        cGameManager(int w,int h){
+            srand(time(NULL));
+            quit = false;
+            up1 = 'w';down1 = 's';
+            up2 = 'i';down2 ='k';
+            score1 = 0; score2 =0;
+            width = w;
+            height = h;
+            ball = new cBall(w/2,h/2);
+            player1 = new cPaddle(1,h/2-3);
+            player2 = new cPaddle(w-2,h/2-3);
+        }
+        ~cGameManager(){
+            delete ball,player1,player2;
+        }
+        void scoreUp(cPaddle *player){
+            if(player == player1) score1++;
+            else if(player == player2) score2++;
+            ball->reset();
+            player1->reset();
+            player2->reset();
+        }
+        void input(){
+
+        }
+        void logic(){
+
+        }
+        void draw(){
+
+        }
 };
 
 int main(){
